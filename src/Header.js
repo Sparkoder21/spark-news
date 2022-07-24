779ad87bc506c82048b6691acf10c26ec037a444
Anuj Chowdhury <109861258+Sparkoder21@users.noreply.github.com>
2022-07-24 20:18:04 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {AppBar, Box, CssBaseline, Typography, Toolbar, IconButton} from "@material-ui/core";
+import {CalendarToday, EventNote, Comment, Info} from "@material-ui/icons";
+
+function Header(props){
+	return (
+		<>
+			<CssBaseline/>
+			<AppBar position="fixed">
+				<Toolbar>
+					<CalendarToday style={{marginBottom: "1.5px", marginLeft: "5px", marginRight: "5px"}}/>
+					<Typography variant="h6"> SparkNews </Typography>
+						<Box position="absolute" right={10}>
+							<IconButton onClick={() => {props.setContainerState("news")}}> 
+								<EventNote style={{color: "white"}}/>
+							</IconButton>
+							<IconButton onClick={() => {props.setContainerState("comments")}}>
+								<Comment style={{color: "white"}}/>
+							</IconButton>
+							<IconButton onClick={() => {props.setContainerState("about")}}>
+								<Info style={{color: "white"}}/>
+							</IconButton>
+						</Box>
+				</Toolbar>
+			</AppBar>
+		</>
+	);
+}
+
+export default Header;
